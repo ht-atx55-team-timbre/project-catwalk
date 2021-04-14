94227c4acfda4d08a6bf15663b85af7ba1448c4b
eric-cruz <eacruz@live.com>
2021-04-14 12:26:34 -0500

    Added Overall Page Layout using MaterialUI Grid

--- src/App.js
+++ src/App.js
@@ -1,9 +1,31 @@
-const App = function() {
-  return (
-    <div>
-      <h1>This is the project!</h1>
-    </div>
-  );
+import React from 'react';
+import { Grid } from '@material-ui/core';
+
+class App extends React.Component {
+  render() {
+    return (
+      <Grid container direction="column">
+        <Grid item>
+          {/* Header component will go here */}
+          <h1>This will be the header</h1>
+        </Grid>
+        <Grid item container>
+          <Grid xs={false} sm={2} />
+          <Grid xs={12} sm={8}>
+            {/* Product Overview */}
+            <h1>This will be for the Product Overview</h1>
+            {/* Related Items */}
+            <h1>This will be for the Related Items</h1>
+            {/* Q/A */}
+            <h1>This will be for the Questions/Answers</h1>
+            {/* Reviews/Ratings */}
+            <h1>This will be for the Reviews/Ratings</h1>
+          </Grid>
+          <Grid xs={false} sm={2} />
+        </Grid>
+      </Grid>
+    )
+  }
 }
 
 export default App;
