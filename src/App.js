9e086ad2c5ee97c2f1d13cc0e65d8e9abeb231a7
Sunil Gadgil <sunilrgadgil@gmail.com>
2021-04-15 16:45:33 -0500

    commit basic boilerplate skeleton for Product Overview module

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import React from 'react';
 import axios from 'axios';
 import { Grid } from '@material-ui/core';
 import API_KEY from './config';
+import ProductOverview from './ProductOverview';
 
 class App extends React.Component {
   // eslint-disable-next-line no-useless-constructor
   constructor(props) {
     super(props)
   }
-  
+
   componentDidMount() {
     axios.get('https://app-hrsei-api.herokuapp.com/api/fec2/hratx/products', {
       headers: {
@@ -30,7 +31,7 @@ class App extends React.Component {
         <Grid item container>
           <Grid item xs={false} sm={2} />
           <Grid item xs={12} sm={8}>
-            {/* Product Overview */}
+            <ProductOverview />
             <h1>This will be for the Product Overview</h1>
             {/* Related Items */}
             <h1>This will be for the Related Items</h1>
---
+++ src/ProductOverview.js
@@ -0,0 +1,64 @@
+import React from 'react';
+import axios from 'axios';
+import { Grid } from '@material-ui/core';
+import sunilAPI from './sunilConfig';
+
+export default class ProductOverview extends React.Component {
+  // eslint-disable-next-line no-useless-constructor
+  constructor(props) {
+    super(props);
+    this.state = {
+      products: [],
+      currentProduct: {}
+    }
+  }
+
+  componentDidMount() {
+    axios.get('https://app-hrsei-api.herokuapp.com/api/fec2/hratx/products', {
+      headers: {
+        Authorization: sunilAPI //hello
+      }
+    })
+      .then(response => {
+        console.log(response);
+        this.setState({
+          products: response.data,
+          currentProduct: response.data[0]
+        })
+      })
+  }
+
+  render() {
+    return (
+      <Grid container direction="column">
+        <Grid container direction="row">
+          <Grid item xs={12} md={8}>
+            <h5>This is where the main product image goes.</h5>
+          </Grid>
+          <Grid container direction="column" xs={12} md={4}>
+            <Grid item xs={12}>
+              <h5>This is where the product info will go (category/title/price, rating). </h5>
+            </Grid>
+            <Grid item xs={12}>
+              <h5>This is where the avavilable styles to choose will go.</h5>
+            </Grid>
+            <Grid item xs={12}>
+              <h5>This is where the cart component will go.</h5>
+            </Grid>
+          </Grid>
+        </Grid>
+        <Grid container direction="row">
+          <Grid item xs={12} md={8}>
+            <h5>This is where the product description will go.</h5>
+          </Grid>
+          <Grid item xs={12} md={4}>
+            <h5>This is where the product specs will go.</h5>
+          </Grid>
+        </Grid>
+      </Grid>
+    );
+  }
+}
+
+// css positioner to allow the float for preview pictures
+// global state should include current product,
