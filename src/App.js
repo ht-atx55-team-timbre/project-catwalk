ad1b4805cf68b2e5c9f7d3eda02c0573c2435f51
eric-cruz <eacruz@live.com>
2021-04-16 10:20:31 -0500

    Completed Realted Product Boilerplate

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import axios from 'axios';
 import { Grid } from '@material-ui/core';
-import API_KEY from './config';
+import Related from './components/related/Related'
 
 class App extends React.Component {
   // eslint-disable-next-line no-useless-constructor
@@ -12,7 +12,7 @@ class App extends React.Component {
   componentDidMount() {
     axios.get('https://app-hrsei-api.herokuapp.com/api/fec2/hratx/products', {
       headers: {
-        Authorization: API_KEY
+        Authorization:''
       }
     })
       .then((results) => {
@@ -32,7 +32,7 @@ class App extends React.Component {
           <Grid item xs={12} sm={8}>
             {/* Product Overview */}
             <h1>This will be for the Product Overview</h1>
-            {/* Related Items */}
+            <Related />
             <h1>This will be for the Related Items</h1>
             {/* Q/A */}
             <h1>This will be for the Questions/Answers</h1>
