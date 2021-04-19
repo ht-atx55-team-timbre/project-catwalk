a21cd4afc0a02cb3fe06f3ba888483cbe947e9f6
Cameron Crow <19ccrow99@gmail.com>
2021-04-19 12:53:34 -0500

    Dynamically rendered all the question/answer data to the page

--- src/qa-components/QAComponent.jsx
+++ src/qa-components/QAComponent.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import { Grid } from '@material-ui/core';
+import _ from 'underscore';
+// import { DateTime } from 'luxon';
 
 // import { makeStyles } from '@material-ui/styles';
 
@@ -9,16 +11,30 @@ import { Grid } from '@material-ui/core';
 //   }
 // }));
 
-const QAComponent = () => {
+const QAComponent = ({ questionsAndAnswers }) => {
   // const classes = useStyles();
+
   return (
     <Grid container>
-      <Grid item xs={6}>
-        <p>Q:</p>
-        <p>A:</p>
+      <Grid item xs={9}>
+        {_.map(questionsAndAnswers, question =>
+          <div key={question.question_id}>
+            <div>
+              <p>{`Q: ${question.question_body}`}</p>
+            </div>
+            <div>
+              {_.map(question.answers, answer =>
+                <div key={answer.id}>
+                  <p>{`A: ${answer.body}`}</p>
+                  <p>{`by ${answer.answerer_name}, ${answer.date}`}</p>
+                </div>
+              )}
+            </div>
+          </div>
+        )}
       </Grid>
-      <Grid item xs={6}>
-        <p>HELPFUL | ADD ANSWER</p>
+      <Grid item xs={3}>
+        <p>Helpful? Yes(25) | Add Answer</p>
       </Grid>
     </Grid>
   )
