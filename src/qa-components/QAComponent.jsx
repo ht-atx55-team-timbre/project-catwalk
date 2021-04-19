ae406e095c4be9191ee8b9c8c0cee16e2d37903b
Cameron Crow <19ccrow99@gmail.com>
2021-04-19 13:25:55 -0500

    Made QAComponent have slightly cleaner code, added the rest of the dynamic data

--- src/qa-components/QAComponent.jsx
+++ src/qa-components/QAComponent.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Grid } from '@material-ui/core';
+import moment from 'moment';
 import _ from 'underscore';
 // import { DateTime } from 'luxon';
 
@@ -15,27 +16,25 @@ const QAComponent = ({ questionsAndAnswers }) => {
   // const classes = useStyles();
 
   return (
-    <Grid container>
-      <Grid item xs={9}>
-        {_.map(questionsAndAnswers, question =>
-          <div key={question.question_id}>
-            <div>
-              <p>{`Q: ${question.question_body}`}</p>
-            </div>
+    <Grid>
+      {_.map(questionsAndAnswers, question =>
+        <Grid container key={question.question_id}>
+          <Grid item xs={9}>
+            <p>{`Q: ${question.question_body}`}</p>
             <div>
               {_.map(question.answers, answer =>
                 <div key={answer.id}>
                   <p>{`A: ${answer.body}`}</p>
-                  <p>{`by ${answer.answerer_name}, ${answer.date}`}</p>
+                  <p>{`by ${answer.answerer_name}, ${moment(answer.date).format('MMMM Do, YYYY')} | Helpful? Yes(${answer.helpfulness}) | Report`}</p>
                 </div>
               )}
             </div>
-          </div>
-        )}
-      </Grid>
-      <Grid item xs={3}>
-        <p>Helpful? Yes(25) | Add Answer</p>
-      </Grid>
+          </Grid>
+          <Grid item xs={3}>
+            <p>{`Helpful? Yes(${question.question_helpfulness}) | Add Answer`}</p>
+          </Grid>
+        </Grid>
+      )}
     </Grid>
   )
 }
