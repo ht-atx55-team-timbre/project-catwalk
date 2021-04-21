9e7596f11cf0a5dfc97653c7a4c170ec5c1eb976
Jeff Jiang <jjiang4566@gmail.com>
2021-04-21 15:02:21 -0500

    delete some console.log and comment

--- src/reviews-components/ReviewCards.js
+++ src/reviews-components/ReviewCards.js
@@ -3,23 +3,10 @@ import React from 'react';
 import ReviewCard from './ReviewCard';
 
 const ReviewCards = ({results}) => {
-  /**
-    body: "I really did or did not like this product based on whether it was sustainably sourced. Then I found out that its made from nothing at all."
-    date: "2019-01-01T00:00:00.000Z"
-    helpfulness: 8
-    photos: []
-    rating: 5
-    recommend: true
-    response: ""
-    review_id: 328556
-    reviewer_name: "funtime"
-    summary: "This product was great!"
-   */
 
   return (
     <>
     {results.map(review => {
-      console.log(review);
       const {body, date, helpfulness, photos, rating, recommend, response, review_id, reviewer_name, summary} = review;
       return (
         <ReviewCard
