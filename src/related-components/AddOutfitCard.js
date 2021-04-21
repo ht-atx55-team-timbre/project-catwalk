18767ed128175eeda6327770c99fb5675a8b9ae1
eric-cruz <eacruz@live.com>
2021-04-21 14:07:14 -0500

    Added button for adding current product to Outfit - no functinality yet

---
+++ src/related-components/AddOutfitCard.js
@@ -0,0 +1,35 @@
+import { RelCard, useStyles } from './CardTemplate';
+import AddCircleOutlineRoundedIcon from '@material-ui/icons/AddCircleOutlineRounded';
+import Container from '@material-ui/core/Container';
+import { IconButton } from '@material-ui/core';
+import Grid from '@material-ui/core/Grid';
+import withStyles from "@material-ui/core/styles/withStyles";
+import CardActions from "@material-ui/core/CardActions";
+
+const AddCardActions = withStyles({
+  root: {
+    position: 'absolute',
+    right: '25%',
+    top: '35%'
+  }
+})(CardActions); 
+
+function AddOutfitCard({ item }) {
+  const classes = useStyles();
+
+  return (
+    <Grid item xs={3}>
+      <Container>
+          <RelCard className={classes.card}>
+            <AddCardActions>
+              <IconButton>
+                <AddCircleOutlineRoundedIcon style={{color: 'black', fontSize: 60}} />
+              </IconButton>
+            </AddCardActions>
+          </RelCard>
+        </Container>
+      </Grid>
+  );
+}
+
+export default AddOutfitCard;
