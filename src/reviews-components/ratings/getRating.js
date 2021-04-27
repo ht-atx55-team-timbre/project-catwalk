d93c23ea38261832f965243ccc0bb1b6cdb08d9f
Jeff Jiang <jjiang4566@gmail.com>
2021-04-27 11:34:48 -0500

    fix rating bug

--- src/reviews-components/ratings/getRating.js
+++ src/reviews-components/ratings/getRating.js
@@ -1,6 +1,9 @@
 const getRating = (ratings) => {
   let totalRating = 0;
   let numberOfRating = 0;
+
+  if (Object.keys(ratings).length === 0) return [0, 0]
+
   for (const key in ratings) {
     totalRating += key * Number(ratings[key]);
     numberOfRating += Number(ratings[key]);
