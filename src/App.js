dc7434bdb18ef60dc0348b004acf9a03bbd7ccbb
Cameron Crow <19ccrow99@gmail.com>
2021-04-29 11:27:55 -0500

    Refractored to hooks

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { Grid, Box } from '@material-ui/core';
 import { MuiThemeProvider } from '@material-ui/core/styles';
@@ -11,78 +11,69 @@ import Related from './related-components/Related';
 import QA from './qa-components/Main.jsx';
 import API_KEY from './config';
 
-class App extends React.Component {
-  // eslint-disable-next-line no-useless-constructor
-  constructor(props) {
-    super(props)
-    this.state = {
-      product_id: null,
-      name: null
-    }
+const App = () => {
+  const [allProducts, setAllProducts] = useState(null);
+  const [product_id, setProduct_id] = useState(null);
+  const [name, setName] = useState(null)
 
-    this.handleProductChange = this.handleProductChange.bind(this);
-  }
-
-  componentDidMount() {
+  useEffect(() => {
     axios.get('https://app-hrsei-api.herokuapp.com/api/fec2/hratx/products', {
       headers: {
         Authorization: API_KEY
       }
     })
       .then((products) => {
-        this.setState({ product_id: products.data[0].id });
-        this.setState({ name: products.data[0].name });
+        setAllProducts(products.data);
+        setProduct_id(products.data[0].id);
+        setName(products.data[0].name);
       })
       .catch((err) => {
         console.log(err, 'error retrieving products from the database');
-      })
-  }
+      });
+  }, []);
 
-  handleProductChange(id, name) {
+  const handleProductChange = (id, name) => {
     this.setState({ product_id: id, name: name });
   }
 
-  render() {
-    if (this.state.product_id) {
-      return (
-        <MuiThemeProvider theme={theme}>
-          <Grid container={true} direction='column'>
-            <Header />
-            <Grid item={true} container={true} direction='row'>
-              <Grid item={true} xs={false} sm={1} />
-              <Grid item={true} xs={12} sm={10}>
-                <ProductOverview product={this.state.product_id} />
-              </Grid>
+  if (product_id) {
+    return (
+      <MuiThemeProvider theme={theme}>
+        <Grid container={true} direction='column'>
+          <Header />
+          <Grid item={true} container={true} direction='row'>
+            <Grid item={true} xs={false} sm={1} />
+            <Grid item={true} xs={12} sm={10}>
+              <ProductOverview allProducts={allProducts} product={product_id} />
+            </Grid>
 
-              <Grid item={true} xs={false} sm={1} />
-            </Grid>
-            <Grid item={true} container={true}>
-              <Grid item={true} xs={false} sm={2} />
-              <Grid item={true} xs={12} sm={8}>
-                <Box pt={3} pb={3}>
-                  <Divider varient='middle'></Divider>
-                </Box>
-                <Related product_id={this.state.product_id} handleIdChange={this.handleProductChange} />
-                {/* Q/A */}
-                <Box pt={3} pb={3}>
-                  <Divider varient='middle'></Divider>
-                </Box>
-                <QA product_id={this.state.product_id} name={this.state.name} />
-                {/* Reviews/Ratings */}
-                <Box pt={3} pb={6}>
-                  <Divider varient='middle'></Divider>
-                </Box>
-                <ReviewsAndRatings product_id={this.state.product_id} name={this.state.name} />
-              </Grid>
-              <Grid item={true} xs={false} sm={2} />
-            </Grid>
+            <Grid item={true} xs={false} sm={1} />
           </Grid>
-        </MuiThemeProvider>
-
-      )
-    } else {
-      return <></>
-    }
+          <Grid item={true} container={true}>
+            <Grid item={true} xs={false} sm={2} />
+            <Grid item={true} xs={12} sm={8}>
+              <Box pt={3} pb={3}>
+                <Divider varient='middle'></Divider>
+              </Box>
+              <Related product_id={product_id} handleIdChange={handleProductChange} />
+              {/* Q/A */}
+              <Box pt={3} pb={3}>
+                <Divider varient='middle'></Divider>
+              </Box>
+              <QA product_id={product_id} name={name} />
+              {/* Reviews/Ratings */}
+              <Box pt={3} pb={6}>
+                <Divider varient='middle'></Divider>
+              </Box>
+              <ReviewsAndRatings product_id={product_id} name={name} />
+            </Grid>
+            <Grid item={true} xs={false} sm={2} />
+          </Grid>
+        </Grid>
+      </MuiThemeProvider>
+    )
+  } else {
+    return <></>
   }
 }
 
