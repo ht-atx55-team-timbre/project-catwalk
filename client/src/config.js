96bed4a826971ba6904b46d61f180e9856787fea
eric-cruz <eacruz@live.com>
2021-05-01 10:12:47 -0500

    Added client folder and separate package.json for server

---
+++ client/src/config.js
@@ -0,0 +1,3 @@
+let API_KEY = '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';
+
+module.exports = API_KEY;
--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const PORT = 3004;
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, 'build')));
+app.use(express.static(path.join(__dirname, 'client/build')));
 
 
 app.all('/*', (req, res) => {
