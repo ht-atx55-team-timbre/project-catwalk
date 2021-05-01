45e63abc245dc53b3bc2c789112b9efea4485fc4
eric-cruz <eacruz@live.com>
2021-05-01 15:41:23 -0500

    Changes to server

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const cors = require('cors');
 
 const app = express();
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3004;
 
 app.use(cors());
 app.use(express.json());
