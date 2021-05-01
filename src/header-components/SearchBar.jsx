1f42d09c93f02dcb2b682401959727711ab4579c
Cameron Crow <19ccrow99@gmail.com>
2021-05-01 09:29:27 -0500

    Modified search bar background color

--- src/header-components/SearchBar.jsx
+++ src/header-components/SearchBar.jsx
@@ -1,7 +1,8 @@
 import React, { useState } from 'react';
 import { TextField } from '@material-ui/core';
 import Autocomplete from '@material-ui/lab/Autocomplete';
-import { makeStyles } from "@material-ui/core/styles";
+import { fade, makeStyles } from "@material-ui/core/styles";
+import theme from '../theme.js';
 
 const useStyles = makeStyles({
   root: {
@@ -9,11 +10,11 @@ const useStyles = makeStyles({
       borderColor: "#181E34"
     },
     "&:hover .MuiOutlinedInput-root .MuiOutlinedInput-notchedOutline": {
-      borderColor: "#181E34"
+      backgroundColor: fade(theme.palette.common.white, 0.15)
     },
     "& .MuiOutlinedInput-root.Mui-focused .MuiOutlinedInput-notchedOutline": {
-      borderColor: "#181E34"
-    }
+      backgroundColor: fade(theme.palette.common.white, 0.15)
+    },
   }
 });
 
